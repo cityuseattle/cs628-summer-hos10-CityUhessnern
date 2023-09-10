f85a6857bdf1703f8bae9538aa8a5601fc19c57f
CityUhessnern <92000484+CityUhessnern@users.noreply.github.com>
2023-09-10 04:06:22 +0000

    Submissior for Module10 - Nikki Hessner

---
+++ frontend/src/components/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <a href="https://improved-adventure-wjjpvg7q75rcgpwp-5050.app.github.dev/facebook"
+        className="btn btn-primary">
+            Login with Facebook
+        </a>
+    );
+};
+
+export default Login;
